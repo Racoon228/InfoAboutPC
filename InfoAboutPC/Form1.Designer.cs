77276a5446353dfe73ca2b704cab7c07a54ae9e8
Vlad Bogush <bog.rac.oon@gmail.com>
2024-12-17 17:51:01 +0300

    Add project files.

---
+++ InfoAboutPC/Form1.Designer.cs
@@ -0,0 +1,102 @@
+﻿namespace InfoAboutPC
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Необходимая переменная дизайнера.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Чистит все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">true, если используется управляемый ресурс; иначе false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный Windows Form Designer
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора - не изменяйте
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            btnGetInfo = new Button();
+            treeViewInfo = new TreeView();
+            chartCPU = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            chartRAM = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            ((System.ComponentModel.ISupportInitialize)(chartCPU)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(chartRAM)).BeginInit();
+            SuspendLayout();
+            // 
+            // btnGetInfo
+            // 
+            this.btnGetInfo.Dock = System.Windows.Forms.DockStyle.Top;
+            btnGetInfo.Location = new Point(14, 14);
+            btnGetInfo.Margin = new Padding(4, 3, 4, 3);
+            btnGetInfo.Name = "btnGetInfo";
+            btnGetInfo.Size = new Size(303, 27);
+            btnGetInfo.TabIndex = 0;
+            btnGetInfo.Text = "Получить информацию о системе";
+            btnGetInfo.UseVisualStyleBackColor = true;
+            btnGetInfo.Click += btnGetInfo_Click;
+            // 
+            // treeViewInfo
+            // 
+            this.treeViewInfo.Dock = System.Windows.Forms.DockStyle.Left;
+            treeViewInfo.Location = new Point(14, 58);
+            treeViewInfo.Margin = new Padding(4, 3, 4, 3);
+            treeViewInfo.Name = "treeViewInfo";
+            treeViewInfo.Size = new Size(400, 500);
+            treeViewInfo.TabIndex = 1;
+            // 
+            // chartCPU
+            // 
+            chartCPU.Dock = DockStyle.Right;
+            chartCPU.Location = new Point(317, 58);
+            chartCPU.Name = "chartCPU";
+            chartCPU.Size = new Size(470, 200);
+            chartCPU.TabIndex = 2;
+            chartCPU.Text = "chartCPU";
+            // 
+            // chartRAM
+            // 
+            chartRAM.Dock = DockStyle.Bottom;
+            chartRAM.Location = new Point(14, 288);
+            chartRAM.Name = "chartRAM";
+            chartRAM.Size = new Size(773, 182);
+            chartRAM.TabIndex = 3;
+            chartRAM.Text = "chartRAM";
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(792, 482);
+            Controls.Add(chartRAM);
+            Controls.Add(treeViewInfo);
+            Controls.Add(chartCPU);
+            Controls.Add(btnGetInfo);
+            Margin = new Padding(4, 3, 4, 3);
+            Name = "Form1";
+            Text = "Утилита для сбора информации о системе";
+            ((System.ComponentModel.ISupportInitialize)(chartCPU)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(chartRAM)).EndInit();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnGetInfo;
+        private System.Windows.Forms.TreeView treeViewInfo; // Объявляем TreeView
+        private System.Windows.Forms.DataVisualization.Charting.Chart chartCPU; // Объявляем график CPU
+        private System.Windows.Forms.DataVisualization.Charting.Chart chartRAM; // Объявляем график RAM
+    }
+}
---
+++ InfoAboutPC/Form1.cs
@@ -0,0 +1,152 @@
+﻿
+using System.Diagnostics;
+using System.Management;
+using System.Net;
+using System.Windows.Forms.DataVisualization.Charting;
+
+namespace InfoAboutPC
+{
+    public partial class Form1 : Form
+    {
+        private PerformanceCounter cpuCounter;
+        private PerformanceCounter ramCounter;
+
+        public Form1()
+        {
+            InitializeComponent();
+            InitializePerformanceCounters();
+            SetupCharts();
+        }
+
+        private void InitializePerformanceCounters()
+        {
+            cpuCounter = new PerformanceCounter("Processor", "% Processor Time", "_Total");
+            ramCounter = new PerformanceCounter("Memory", "Available MBytes");
+        }
+
+        private void SetupCharts()
+        {
+            chartCPU.Series.Clear();
+            var seriesCPU = new Series("CPU Usage")
+            {
+                ChartType = SeriesChartType.Line,
+                Color = Color.Red,
+                BorderWidth = 2,
+                IsValueShownAsLabel = true
+            };
+            chartCPU.Series.Add(seriesCPU);
+
+            chartRAM.Series.Clear();
+            var seriesRAM = new Series("Available RAM")
+            {
+                ChartType = SeriesChartType.Line,
+                Color = Color.Blue,
+                BorderWidth = 2,
+                IsValueShownAsLabel = true
+            };
+            chartRAM.Series.Add(seriesRAM);
+        }
+
+        private void btnGetInfo_Click(object sender, EventArgs e)
+        {
+            treeViewInfo.Nodes.Clear();
+            GetSystemInfo();
+            UpdateCharts();
+        }
+
+        private void GetSystemInfo()
+        {
+            var systemInfoNode = new TreeNode("Информация о системе");
+
+            var osNode = new TreeNode("Операционная система");
+            using (ManagementObjectSearcher searcher = new ManagementObjectSearcher("select * from Win32_OperatingSystem"))
+            {
+                foreach (ManagementObject obj in searcher.Get())
+                {
+                    osNode.Nodes.Add($"Операционная система: {obj["Caption"]}\n");
+                }
+            }
+            systemInfoNode.Nodes.Add(osNode);
+
+            var userInfoNode = new TreeNode("Информация о пользователе");
+            userInfoNode.Nodes.Add($"Имя компьютера: {Environment.MachineName}\n");
+            userInfoNode.Nodes.Add($"Пользователь: {Environment.UserName}\n");
+            systemInfoNode.Nodes.Add(userInfoNode);
+
+            var processorNode = new TreeNode("Процессор");
+            using (ManagementObjectSearcher searcher = new ManagementObjectSearcher("select * from Win32_Processor"))
+            {
+                foreach (ManagementObject obj in searcher.Get())
+                {
+                    processorNode.Nodes.Add($"Процессор: {obj["Name"]}\n");
+                }
+            }
+            systemInfoNode.Nodes.Add(processorNode);
+
+            var memoryNode = new TreeNode("Оперативная память");
+            using (ManagementObjectSearcher searcher = new ManagementObjectSearcher("select * from Win32_PhysicalMemory"))
+            {
+                long totalMemory = 0;
+                foreach (ManagementObject obj in searcher.Get())
+                {
+                    totalMemory += Convert.ToInt64(obj["Capacity"]);
+                }
+                memoryNode.Nodes.Add($"Оперативная память: {totalMemory / (1024 * 1024)} MB\n");
+            }
+            systemInfoNode.Nodes.Add(memoryNode);
+
+            string ipAddress = GetLocalIPAddress(); systemInfoNode.Nodes.Add($"IP-адрес: {ipAddress}\n");
+
+            var disksNode = new TreeNode("Диски");
+            foreach (var drive in DriveInfo.GetDrives())
+            {
+                if (drive.IsReady)
+                {
+                    var driveNode = new TreeNode($"Диск: {drive.Name}");
+                    driveNode.Nodes.Add($"   Общая емкость: {drive.TotalSize / (1024 * 1024 * 1024)} GB\n");
+                    driveNode.Nodes.Add($"   Используемая емкость: {(drive.TotalSize - drive.AvailableFreeSpace) / (1024 * 1024 * 1024)} GB\n");
+                    driveNode.Nodes.Add($"   Свободная емкость: {drive.AvailableFreeSpace / (1024 * 1024 * 1024)} GB\n");
+
+                    disksNode.Nodes.Add(driveNode);
+                }
+            }
+            systemInfoNode.Nodes.Add(disksNode);
+            treeViewInfo.Nodes.Add(systemInfoNode);
+        }
+
+        private void UpdateCharts()
+        {
+            float cpuUsage = cpuCounter.NextValue();
+            float availableRam = ramCounter.NextValue();
+
+            chartCPU.Series["CPU Usage"].Points.AddY(cpuUsage);
+
+            chartRAM.Series["Available RAM"].Points.AddY(availableRam);
+
+            treeViewInfo.Nodes[0].Nodes.Add($"Текущая загрузка CPU: {cpuUsage}%");
+            treeViewInfo.Nodes[0].Nodes.Add($"Доступная RAM: {availableRam} MB");
+        }
+
+        private string GetLocalIPAddress()
+        {
+            string localIP = "Не удалось получить IP-адрес";
+            try
+            {
+                var host = Dns.GetHostEntry(Dns.GetHostName());
+                foreach (var ip in host.AddressList)
+                {
+                    if (ip.AddressFamily == System.Net.Sockets.AddressFamily.InterNetwork)
+                    {
+                        localIP = ip.ToString();
+                        break;
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                localIP = ex.Message;
+            }
+            return localIP;
+        }
+    }
+}
